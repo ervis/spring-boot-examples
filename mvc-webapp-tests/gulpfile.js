61c68109db006d3279e2ee9d5473182eab08b5df
Ervis Zyka <erviszyka@gmail.com>
2020-04-05 20:41:11 +0300

    add javascript linter
    - add babel to convert es2015 to es5
    - use eslint for JS code quality

--- mvc-webapp-tests/gulpfile.js
+++ mvc-webapp-tests/gulpfile.js
@@ -1,54 +1,107 @@
 const gulp = require('gulp');
-const watch = require('gulp-watch');
+const babel = require('gulp-babel');
 const sass = require('gulp-sass');
 const sassLint = require('gulp-sass-lint');
+const eslint = require('gulp-eslint');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 
+// TODO: minify and uglify
+const minify = require('gulp-babel-minify');
+const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
+
+// is this one used?
+const watch = require('gulp-watch');
+
+const server = 'localhost:8081';
+
 const paths = {
-    resources: 'src/main/resources',
-    css: 'src/main/resources/static/assets/app/css',
-    scss: 'src/scss'
+    resources: 'src/main/resources'
 };
+
 const styles = {
-    file: 'main.css'
+    file: 'main.css',
+    input: 'src/scss',
+    output: 'src/main/resources/static/assets/app/css'
 };
+
+const js = {
+    fileLocation: 'src/main/resources/static/assets/app/js/main.js',
+    file: 'main.js',
+    input: 'src/js',
+    output: 'src/main/resources/static/assets/app/js'
+};
+
 const build = {
+    // replace with build/resources/main/ for netBeans
+    // or classpath: 'out/production/resources/'
+    // this is the only one that works on IDEA
     classpath: 'build/src/main'
 };
-gulp.task('watch', () => {
-    browserSync.init({proxy: 'localhost:8081',});
-    gulp.watch([`${paths.scss}/**`], gulp.series('sass'));
-    gulp.watch([`${paths.resources}/**`], gulp.series('copy-resources-and-reload'));
+
+const task = {
+    babel: 'babel',
+    build: 'build',
+    minifyjs: 'minifyjs',
+    default: 'default',
+    sass: 'sass',
+    sassLinter: 'sass-lint',
+    eslint: 'js-lint',
+    watch: 'watch',
+    copyAndReload: 'copy-resources-and-reload',
+    copyResources: 'copy-resources',
+};
+
+gulp.task(task.watch, () => {
+    browserSync.init({proxy: server});
+    gulp.watch([`${styles.input}/**`], gulp.series(task.sass));
+    gulp.watch([`${js.input}/**`], gulp.series(task.babel));
+    gulp.watch([`${paths.resources}/**`], gulp.series(task.copyAndReload));
 });
 
-gulp.task('sass', () => {
-    return gulp.src(`${paths.scss}/**/*.s+(a|c)ss`)
+gulp.task(task.sass, () => {
+    return gulp.src(`${styles.input}/**/*.s+(a|c)ss`)
         .pipe(sassLint())
         .pipe(sassLint.format())
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(concat(styles.file))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css));
+        .pipe(gulp.dest(styles.output));
+});
+
+gulp.task(task.babel, () => {
+    return gulp.src(`${js.input}/**/*.js`)
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat(js.file))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(js.output));
 });
 
 // used for code quality
-gulp.task('sass-lint', () => {
-    return gulp.src(`${paths.scss}/**/*.s+(a|c)ss`)
+gulp.task(task.sassLinter, () => {
+    return gulp.src(`${styles.input}/**/*.s+(a|c)ss`)
         .pipe(sassLint())
         .pipe(sassLint.format())
         .pipe(sassLint.failOnError());
 });
 
-//replace with build/resources/main/ for netBeans
-//.pipe(gulp.dest('out/production/resources/'));
-gulp.task('copy-resources', () => gulp.src([`${paths.resources}/**`]).pipe(gulp.dest(build.classpath)));
+// used for code quality
+gulp.task(task.eslint, () => {
+    return gulp.src(`${js.input}/**/*.js`)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+});
 
-gulp.task('copy-resources-and-reload', gulp.series('copy-resources', reload));
-gulp.task('build', gulp.series('copy-resources',));
-gulp.task('default', gulp.series('watch'));
+gulp.task(task.copyResources, () => gulp.src([`${paths.resources}/**`]).pipe(gulp.dest(build.classpath)));
+
+gulp.task(task.copyAndReload, gulp.series(task.copyResources, reload));
+gulp.task(task.build, gulp.series(task.copyResources,));
+gulp.task(task.default, gulp.series(task.watch));
 
 function reload(done) {
     browserSync.reload();
